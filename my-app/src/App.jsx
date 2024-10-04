18f50d22b1cc0ead6abc198852b58090f7facddd
estefania-zakovicz <estefaniazakovicz@hotmail.com>
2024-10-04 20:54:02 -0300

    cambios

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Tables from './Components/Tables';
-/* import Form from './Components/Form'; */
 
 const App = () => {
   return (
     <div className="App">
       <h1 className="text-center text-2xl font-bold">Registro de Clientes</h1>
-      {/* <Form /> */}
+     
       <Tables />
     </div>
   );
